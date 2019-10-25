5f554b29c5c2fc8b7abd20e42e0eb3af1759e493
Sebastian <sego@live.dk>
2019-10-25 14:41:53 +0200

    printInfo is fixed to Teacher Implementation

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,7 +1,11 @@
+import Model.Teacher;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Tis nothing but a flesh wound");
+        Teacher teacherOne = new Teacher();
+        teacherOne.printInfo();
     }
 }
--- App/src/Model/Teacher.java
+++ App/src/Model/Teacher.java
@@ -4,32 +4,28 @@ package Model;
 import java.util.ArrayList;
 
 // Sego's teacher class
-public class Teacher{
+public class Teacher {
 
     /**
-     *
-     *
-     *  Remember to make at least one constructor (and consider whether there is need for more)
-     *
-     *  Consider the class variables needed
-     *
-     *  If you are creating a new method, think about whether it should be used from outside the class or not;
-     *  If not, make it private. Don't let other classes modify sensitive information for your class.
-     * 
-     *
-     *  Some methods return arrays now. They can also be changed to return ArrayLists instead, but
-     *  you have to decide together with the team on either one or the other, so there is consistency.
-     *
-     *
-     *  Talk to each other! Remember your implementations should work together. The system is not finished until
-     *  everything is finished.
-     *
-     *
+     * Remember to make at least one constructor (and consider whether there is need for more)
+     * <p>
+     * Consider the class variables needed
+     * <p>
+     * If you are creating a new method, think about whether it should be used from outside the class or not;
+     * If not, make it private. Don't let other classes modify sensitive information for your class.
+     * <p>
+     * <p>
+     * Some methods return arrays now. They can also be changed to return ArrayLists instead, but
+     * you have to decide together with the team on either one or the other, so there is consistency.
+     * <p>
+     * <p>
+     * Talk to each other! Remember your implementations should work together. The system is not finished until
+     * everything is finished.
+     * <p>
+     * <p>
      * The printInfo() method should print a readable, easily to understand String representation of the specific instance of the class,
      * preferably on several lines.
-     *
-     *
-     * **/
+     **/
 
     //variables: name, age
     private String name = "";
@@ -40,47 +36,53 @@ public class Teacher{
     private ArrayList<Student> students = new ArrayList<>();
 
     //constructors
-    public Teacher(){
+    public Teacher() {
         this.name = "no name";
     }
-    public  Teacher(String newName, int newAge){
+
+    public Teacher(String newName, int newAge) {
         this.name = newName;
         this.age = newAge;
     }
 
-    public void setName(String newName){
+    public void setName(String newName) {
         this.name = newName;
     }
 
-    public void setAge(int newAge){
+    public void setAge(int newAge) {
         this.age = newAge;
     }
 
-    public ArrayList<Course> getCoursesTaught(){
+    public ArrayList<Course> getCoursesTaught() {
         return courses;
     }
 
-    public ArrayList<Course> getPreviousCoursesTaught(){
+    public ArrayList<Course> getPreviousCoursesTaught() {
         return courses;
     }
 
-    public ArrayList<Student> getAllStudentTaught(){
+    public ArrayList<Student> getAllStudentTaught() {
         return students;
     }
 
 
-
-    public void gradeToStudent(Student student){
+    public void gradeToStudent(Student student) {
 
     }
 
-    public void printInfo(){
-        System.out.print(this.name + " teaches in: " + this.courses.get(1).getName());
-        for(int i = 1; i < getCoursesTaught().length; i++){
-            System.out.print(", " + this.courses.get(i).getName());
+    public void printInfo() {
+        System.out.print(this.name + " teaches in: " + this.courses.get(0).getCourseName());
+        for (int i = 1; i < courses.size(); i++) {
+            System.out.print(", " + this.courses.get(i).getCourseName());
         }
-        System.out.println(" on " + this.universities.getName() + ". ");
-        System.out.println("All students taught are: " + this.getAllStudentTaught());
+        System.out.println("On these universities: ");
+        for (int i = 0; i < universities.size(); i++) {
+            universities.get(i).printInfo();
+        }
+        System.out.println("All students taught are: ");
+        for (int i = 0; i < students.size(); i++) {
+            students.get(i).printInfo();
+        }
         System.out.println("------------------------------------------------------");
         System.out.println("more info to come... waiting for setup");
     }
